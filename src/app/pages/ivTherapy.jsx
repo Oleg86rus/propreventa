64f727ac04c4c2126852e6b178f8889a284c7cd4
oleg086rus <o.pototskiy@yandex.ru>
2022-07-16 20:21:27 +0500

    route

---
+++ src/app/pages/ivTherapy.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/ui/navBar/navBar';
+
+const ivTerapy = () => {
+  
+  return (
+    <>
+      <Navbar/>
+      <h1>IV THERAPY</h1>
+    </>
+  );
+};
+
+export default ivTerapy;
