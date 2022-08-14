1f39d092311a83232e4ee75f5a24bb52682aed5f
oleg086rus <o.pototskiy@yandex.ru>
2022-08-14 14:48:47 +0500

    fix

--- client/src/app/components/ui/mainPage/specialOffer/specialOffer.jsx
+++ client/src/app/components/ui/mainPage/specialOffer/specialOffer.jsx
@@ -18,27 +18,28 @@ const Slider = () => {
   }, 1000);
   return (
     <>
-      <div className='overflow-auto mt-10 mb-10'>
-        <h1 className={h1}>Акции</h1>
-        <div className=' relative rounded-lg w-full h-full border-2 border-amber-600'>
-          <div className='w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-[10px] overflow-hidden xl:aspect-w-7 xl:aspect-h-8'>
-            <Swiper
-              loop={true}
-              modules={[Navigation, Autoplay]}
-              spaceBetween={50}
-              slidesPerView={1}
-              navigation
-              autoplay='300'
-            >
-              {promo.map( (el, i) => (
-                <SwiperSlide key={i}>
-                  <img className='w-full h-full object-center object-cover' src={el.imageSrc} alt={el.imageAlt}/>
-                </SwiperSlide>
-              ))}
-            </Swiper>
+      {!promo ? <Loader/>
+        : <div className='overflow-auto mt-10 mb-10'>
+          <h1 className={h1}>Акции</h1>
+          <div className=' relative rounded-lg w-full h-full border-2 border-amber-600'>
+            <div className='w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-[10px] overflow-hidden xl:aspect-w-7 xl:aspect-h-8'>
+              <Swiper
+                loop={true}
+                modules={[Navigation, Autoplay]}
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                autoplay='300'
+              >
+                {promo.map( (el, i) => (
+                  <SwiperSlide key={i}>
+                    <img className='w-full h-full object-center object-cover' src={el.imageSrc} alt={el.imageAlt}/>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
           </div>
-        </div>
-      </div>
+        </div>}
     </>
   );
 };
